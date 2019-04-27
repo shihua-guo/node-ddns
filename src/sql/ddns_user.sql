9c590084873e0795449712cb1405d44882373393
alan9uo <shihua-guo@hotmail.com>
2019-04-27 11:05:43 +0800

    add sql files

---
+++ src/sql/ddns_user.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `ddns_user`  (
+  `token` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `domain` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+)
---
+++ src/sql/tencent_config.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `tencent_config`  (
+  `SecretId` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `SecretKey` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `domain` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
+)
